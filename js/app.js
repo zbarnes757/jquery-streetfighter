2ae1992fc15e64afc1466653650e2588ada1adb6
Zac Barnes <zbarnes@qubiclemonkey.com>
2014-06-21 23:48:18 -0300

    javascript added
    added javascript

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+	$('.ryu').mouseenter(function() {
+	    $('.ryu-still').hide();
+		$('.ryu-ready').show();
+  })
+	.mouseleave(function() {
+	    $('.ryu-still').show();
+		$('.ryu-ready').hide();
+  })
+	.mousedown(function() {
+    	playHadouken ();
+	    $('.ryu-ready').hide();
+	    $('.ryu-throwing').show();
+	    $('.hadouken').finish().show()
+	    .animate(
+	    	{'left': '300px'},
+		  	500,
+		  	function() {
+		    	$(this).hide();
+		    	$(this).css('left', '-200px');
+		  	}
+		);
+  })
+  	.mouseup(function() {
+    	$('.ryu-throwing').hide();
+  		$('.ryu-ready').show();
+  });
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+};
